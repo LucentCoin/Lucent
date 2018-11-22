8bd2eeebcfb114e02219a7e9ec9bb3d50803ebc4
Lucent <>
2018-11-22 22:55:28 +0530

    Update header message

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -185,7 +185,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xbf;
+        pchMessageStart[0] = 0xd3;
         pchMessageStart[1] = 0x0c;
         pchMessageStart[2] = 0x6b;
         pchMessageStart[3] = 0xbd;
